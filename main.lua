dbfee2d295157e2cc40d6efc9d31c129a40c83bb
Edward Wenschhof <edwenschhof@gmail.com>
2025-08-25 17:15:15 -0400

    Initial commit.

---
+++ main.lua
@@ -0,0 +1,12 @@
+function love.load()
+    love.window.setTitle('German Asteroid Game')
+    love.graphics.setBackgroundColor(255, 255, 255)
+    love.window.setFullscreen(true)
+end
+
+-- Function for debugging for now. Allows me to quit a fullscreen window
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
