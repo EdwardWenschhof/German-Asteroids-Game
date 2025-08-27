c13a7c2799cdcd170993804dfc0cf8719b5e557a
Edward Wenschhof <edwenschhof@gmail.com>
2025-08-27 13:45:34 -0400

    Add logic to add a new asteroid to the game

--- game.lua
+++ game.lua
@@ -1,9 +1,19 @@
+local Asteroid = require('asteroid')
+
 local Game = {}
 Game.__index = Game
 
 function Game:new()
     local game = setmetatable({}, self)
+    self.asteroids = {}
+    self.numAsteroids = 0
     return game
+end
+
+function Game:createNewAsteroid()
+    self.numAsteroids = self.numAsteroids + 1
+    local a = Asteroid:new()
+    table.insert(self.asteroids, self.numAsteroids, a)
 end
 
 return Game
