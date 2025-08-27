b8d30dd0c67dd8da260cb5902cb756e0289472d7
Edward Wenschhof <edwenschhof@gmail.com>
2025-08-27 13:02:59 -0400

    Create logic to draw asteroid

--- main.lua
+++ main.lua
@@ -1,7 +1,11 @@
+local Asteroid = require('asteroid')
+local a
+
 function love.load()
     love.window.setTitle('German Asteroid Game')
     love.graphics.setBackgroundColor(0, 0, 0)
     love.window.setFullscreen(true)
+    a = Asteroid:new('hello')
 end
 
 -- Function for debugging for now. Allows me to quit a fullscreen window
@@ -9,4 +13,8 @@ function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
     end
+end
+
+function love.draw()
+    a:draw()
 end
