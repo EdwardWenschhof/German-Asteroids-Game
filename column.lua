c6d9fbaa5984134d239c3a644a1a5b625b912a10
Edward Wenschhof <edwenschhof@gmail.com>
2025-08-27 15:23:50 -0400

    Add fields to column class

--- column.lua
+++ column.lua
@@ -1,7 +1,13 @@
 local Column = {}
 Column.__index = Column
 
-function Column:new()
+function Column:new(center, radius)
+    local column = setmetatable({}, self)
+    column.center = center
+    column.radius = radius
+    column.leftExtent = center - radius
+    column.rightExtent = center + radius
+    return column
 end
 
 return Column
