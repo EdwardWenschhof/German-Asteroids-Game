92055e95ff8f6431054aa2ea2fafde50afc28835
Edward Wenschhof <edwenschhof@gmail.com>
2025-08-27 14:27:39 -0400

    Refactor new function; add dynamic size of asteroids

--- asteroid.lua
+++ asteroid.lua
@@ -3,18 +3,24 @@ Asteroid.__index = Asteroid
 
 function Asteroid:new(asteroidNumber, speed)
     local asteroid = setmetatable({}, self)
-    -- asteroid.word = word
-    asteroid.startX = self:getStartingPos()
-    asteroid.currY = 0
+    local w, h = love.graphics.getDimensions()
+
     asteroid.asteroidNumber = asteroidNumber
     asteroid.speed = speed
+
+    asteroid.w = w
+    asteroid.h = h
+    asteroid.radius = asteroid.w * .04
+    asteroid.startX = asteroid:getStartingPos()
+    asteroid.currY = 0
+
     return asteroid
 end
 
 -- might replace later with sprite
 function Asteroid:draw()
     love.graphics.setColor(.64, .64, .64)
-    love.graphics.circle('fill', self.startX, self.currY, 50)
+    love.graphics.circle('fill', self.startX, self.currY, self.radius)
     love.graphics.setColor(0, 0, 0)
 end
 
@@ -23,8 +29,7 @@ function Asteroid:update(dt)
 end
 
 function Asteroid:getStartingPos()
-    local w, h = love.graphics.getDimensions()
-    local startW = love.math.random(0, w)
+    local startW = love.math.random(0, self.w)
     return startW
 end
 
