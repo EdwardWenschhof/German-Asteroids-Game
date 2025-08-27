fecc73b17079a973ca957adafa41fd6ee0aa62fa
Edward Wenschhof <edwenschhof@gmail.com>
2025-08-27 13:49:16 -0400

    Add logic to destroy an asteroid (remove it from the list)

--- game.lua
+++ game.lua
@@ -7,13 +7,18 @@ function Game:new()
     local game = setmetatable({}, self)
     self.asteroids = {}
     self.numAsteroids = 0
+    self.asteroidSpeed = 50
     return game
 end
 
 function Game:createNewAsteroid()
     self.numAsteroids = self.numAsteroids + 1
-    local a = Asteroid:new()
+    local a = Asteroid:new(self.numAsteroids)
     table.insert(self.asteroids, self.numAsteroids, a)
+end
+
+function Game:destroyAsteroid(asteroid)
+    table.remove(asteroid.asteroidNumber)
 end
 
 return Game
