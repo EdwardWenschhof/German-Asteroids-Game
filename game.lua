f9d47f65ccd3ccdd34fa299d830e09bbaffda8f4
Edward Wenschhof <edwenschhof@gmail.com>
2025-08-27 13:53:18 -0400

    Create logic for setting asteroid speed

--- game.lua
+++ game.lua
@@ -7,13 +7,17 @@ function Game:new()
     local game = setmetatable({}, self)
     self.asteroids = {}
     self.numAsteroids = 0
-    self.asteroidSpeed = 50
+    self.asteroidSpeeds = {50, 75, 100}
     return game
 end
 
 function Game:createNewAsteroid()
+    -- choose random asteroid speed
+    local speedChoice = love.math.random(1, 3)
+    local speed = self.asteroidSpeeds[speedChoice]
+
     self.numAsteroids = self.numAsteroids + 1
-    local a = Asteroid:new(self.numAsteroids)
+    local a = Asteroid:new(self.numAsteroids, speed)
     table.insert(self.asteroids, self.numAsteroids, a)
 end
 
