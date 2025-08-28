cc1a176da3f104e0ef729533a7064d2849e3c8b9
Edward Wenschhof <edwenschhof@gmail.com>
2025-08-28 01:06:19 -0400

    Add math.floor to make text less blurry

--- interface.lua
+++ interface.lua
@@ -15,7 +15,7 @@ function Interface:drawBoard()
     love.graphics.line(0, self.height - self.typeSpace, self.width, self.height - self.typeSpace)
     love.graphics.line((1/3) * self.width, self.height, (1/3) * self.width, self.height - self.typeSpace)
     love.graphics.line((2/3) * self.width, self.height, (2/3) * self.width, self.height - self.typeSpace)
-    love.graphics.print(self.boxText, (1/3) * self.width, self.height - (self.typeSpace) / 2)
+    love.graphics.print(self.boxText, math.floor((1/3) * self.width), math.floor(self.height - (self.typeSpace) / 2))
     love.graphics.setColor(0, 0, 0)
 end
 
