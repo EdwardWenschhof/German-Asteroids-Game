0c008f15ba2ed9afc05dba5f4d80743dee3e6f2b
Edward Wenschhof <edwenschhof@gmail.com>
2025-09-26 22:53:57 -0400

    Refactor text entry box logic out of interface class

---
+++ gui/entrybox.lua
@@ -0,0 +1,42 @@
+local EntryBox = {}
+EntryBox.__index = EntryBox
+
+function EntryBox:new(w, h, startY)
+    local entryBox = setmetatable({}, self)
+    entryBox.text = ""
+    entryBox.width = w
+    entryBox.height = h
+    entryBox.startY = startY
+    return entryBox
+end
+
+function EntryBox:drawLines()
+    love.graphics.setColor(1, 1, 1)
+
+    love.graphics.line((1/3) * self.width, self.height, (1/3) * self.width, self.startY)
+    love.graphics.line((2/3) * self.width, self.height, (2/3) * self.width, self.startY)
+
+    love.graphics.setColor(0, 0, 0)
+end
+
+function EntryBox:drawText()
+    love.graphics.setColor(1, 1, 1)
+    local textY = self.height - ((self.height - self.startY) / 2)
+    love.graphics.printf(self.text, math.floor((1/3) * self.width), textY, self.width * (1/3), "center")
+    love.graphics.setColor(0, 0, 0)
+end
+
+function EntryBox:draw()
+    self:drawLines()
+    self:drawText()
+end
+
+function EntryBox:textinput(t)
+    local newText = self.text .. t
+    local textWidth = love.graphics.getFont():getWidth(newText)
+    if textWidth <= (1/3) * self.width then
+        self.text = newText
+    end
+end
+
+return EntryBox
--- interface.lua
+++ interface.lua
@@ -1,3 +1,5 @@
+local EntryBox = require('gui.entrybox')
+
 local Interface = {}
 Interface.__index = Interface
 
@@ -6,26 +8,20 @@ function Interface:new(w, h)
     interface.width = w
     interface.height = h
     interface.typeSpace = (1/8) * h
-    interface.boxText = ""
+    interface.entryBox = EntryBox:new(w, h, h * (7/8))
     return interface
 end
 
 function Interface:drawBoard()
     love.graphics.setColor(1, 1, 1)
-
     love.graphics.line(0, self.height - self.typeSpace, self.width, self.height - self.typeSpace)
-    love.graphics.line((1/3) * self.width, self.height, (1/3) * self.width, self.height - self.typeSpace)
-    love.graphics.line((2/3) * self.width, self.height, (2/3) * self.width, self.height - self.typeSpace)
-    love.graphics.printf(self.boxText, math.floor((1/3) * self.width), math.floor(self.height - (self.typeSpace) / 2), self.width * (1/3), "center")
+    self.entryBox:drawText()
+    self.entryBox:drawLines()
     love.graphics.setColor(0, 0, 0)
 end
 
 function Interface:textinput(t)
-    local newText = self.boxText .. t
-    local textWidth = love.graphics.getFont():getWidth(newText)
-    if textWidth <= (1/3) * self.width then
-        self.boxText = newText
-    end
+    self.entryBox:textinput(t)
 end
 
 return Interface
