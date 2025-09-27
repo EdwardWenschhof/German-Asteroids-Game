5307c03a617de785611f850606507e4147f6cc3f
Edward Wenschhof <edwenschhof@gmail.com>
2025-09-26 22:54:28 -0400

    Begin to refactor management of asteroids out of game class

---
+++ asteroidmanager.lua
@@ -0,0 +1,51 @@
+local Asteroid = require('asteroid')
+
+local AsteroidManager = {}
+AsteroidManager.__index = AsteroidManager
+
+function AsteroidManager:new(totalAsteroids)
+    local asteroidManager = setmetatable({}, self)
+    asteroidManager.asteroids = {}
+    asteroidManager.numAsteroids = 0
+    asteroidManager.asteroidSpeeds = {50, 100, 200, 250, 275}
+    asteroidManager.totalAsteroids = totalAsteroids
+    return asteroidManager
+end
+
+function AsteroidManager:createAsteroids()
+    while self.numAsteroids < self.totalAsteroids do
+        self:createNewAsteroid()
+    end
+end
+
+function AsteroidManager:createNewAsteroid()
+    -- choose random asteroid speed
+    local speedChoice = love.math.random(#self.asteroidSpeeds)
+    local speed = self.asteroidSpeeds[speedChoice]
+
+    self.numAsteroids = self.numAsteroids + 1
+
+    local col = self:chooseColumn()
+    
+    local a = Asteroid:new(self.numAsteroids, speed, self.radius, col.center)
+    a.column = col
+    table.insert(self.asteroids, self.numAsteroids, a)
+end
+
+function AsteroidManager:destroyAsteroid(asteroid)
+    if asteroid and asteroid.asteroidNumber then
+        if asteroid.column then
+            asteroid.column.occupied = false
+        end
+        table.remove(self.asteroids, asteroid.asteroidNumber)
+        self.numAsteroids = math.max(0, self.numAsteroids - 1)
+        -- reindex remaining asteroids so they start at 1
+        for i = 1, self.numAsteroids do
+            if self.asteroids[i] then
+                self.asteroids[i].asteroidNumber = i
+            end
+        end
+    end
+end
+
+return AsteroidManager
